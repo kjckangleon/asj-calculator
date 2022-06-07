94c6ec2a2179aec57fa6a9a5012ac15bd0a162de
karl-kangleon <kangleon.kjc@sprobe.com>
2022-06-08 00:34:57 +0800

    Change formula again

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -133,7 +133,7 @@ const Calculator = () => {
                 </TableRow>
                 <TableRow>
                   <TableCell>Gain %</TableCell>
-                  <TableCell>{Number((newBalance/value).toString().match(/^\d+(?:\.\d{0,2})?/))*100}</TableCell>
+                  <TableCell>{Number(((newBalance/value)*100).toString().match(/^\d+(?:\.\d{0,2})?/))}</TableCell>
                 </TableRow>
               </TableBody>
             </Table>
